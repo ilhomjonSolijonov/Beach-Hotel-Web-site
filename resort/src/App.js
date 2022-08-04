1280089eb8cd2cfebab374d44349e076e253cac4
ilhomjonSolijonov <ilhomjon206solijonov@gmail.com>
2022-08-04 13:09:23 +0500

    in Router has promblems so i must solve it

--- resort/src/App.js
+++ resort/src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
+
 import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
-import Error from './pages/Error'
+// import Error from './pages/Error'
+
+import {Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
    <>
-      <Home/>
-      <Rooms/>
-      <SingleRoom/>
-      <Error/>
+   <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path='/room' component={Rooms}/>
+      <Route exact path='/room/:slug' component={SingleRoom}/>
+   </Switch>
+    
    </>
   );
 }
