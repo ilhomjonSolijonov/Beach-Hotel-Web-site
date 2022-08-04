8cdd1424a33c7cb2e026453bab2427f37534b095
ilhomjonSolijonov <ilhomjon206solijonov@gmail.com>
2022-08-04 12:30:57 +0500

    Add main pages and link to App.js

---
+++ resort/src/pages/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>Hello from Error</div>
+  )
+}
+
+export default Error
---
+++ resort/src/pages/Rooms.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Rooms = () => {
+  return (
+    <div>Hello from Rooms page</div>
+  )
+}
+
+export default Rooms
